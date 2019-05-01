8feda2644de1b9412cfa5da3828f039ce3e4c94b
Leammin <leammin@outlook.com>
2019-05-01 18:42:40 +0800

    删除无用代码

--- src/main/java/com/leammin/leetcode/util/AlgorithmUtils.java
+++ src/main/java/com/leammin/leetcode/util/AlgorithmUtils.java
@@ -5,27 +5,4 @@ package com.leammin.leetcode.util;
  * @date 2018-11-04
  */
 public class AlgorithmUtils {
-//    public static long time(Runnable runnable) {
-//        long before = System.nanoTime();
-//        runnable.run();
-//        return System.nanoTime() - before;
-//    }
-//
-//    public static int fastest(Runnable... runnables) {
-//        if (runnables == null || runnables.length == 0) {
-//            throw new IllegalArgumentException();
-//        }
-//        int fastestIndex = -1;
-//        long fastestTime = Long.MAX_VALUE;
-//        for (int i = 0; i < runnables.length; i++) {
-//            long time = time(runnables[i]);
-//            if (time < fastestTime) {
-//                fastestIndex = i;
-//                fastestTime = time;
-//            }
-//            System.out.println(String.format("index:%d, time:%.6fms", i, time / 1000000.0));
-//        }
-//        System.out.println(String.format("fastest index:%d, fastest time:%.6fms", fastestIndex, fastestTime / 1000000.0));
-//        return fastestIndex;
-//    }
 }
