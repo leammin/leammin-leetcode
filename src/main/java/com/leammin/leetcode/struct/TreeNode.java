2aaca4357b4ab63c4ff23882b18efb83b990528a
Leammin <leammin@outlook.com>
2019-05-01 14:03:40 +0800

    ListNode of

--- src/main/java/com/leammin/leetcode/struct/TreeNode.java
+++ src/main/java/com/leammin/leetcode/struct/TreeNode.java
@@ -17,8 +17,12 @@ public class TreeNode {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         TreeNode treeNode = (TreeNode) o;
         return val == treeNode.val &&
                 Objects.equals(left, treeNode.left) &&
