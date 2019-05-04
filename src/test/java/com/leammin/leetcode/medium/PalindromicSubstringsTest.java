e0c98d66a0dd9a012991f55632231f26c9dbcb2a
Leammin <leammin@outlook.com>
2019-05-04 15:23:33 +0800

    回文子串 增加测试用例

--- src/test/java/com/leammin/leetcode/medium/PalindromicSubstringsTest.java
+++ src/test/java/com/leammin/leetcode/medium/PalindromicSubstringsTest.java
@@ -26,12 +26,16 @@ class PalindromicSubstringsTest extends AbstractTest<PalindromicSubstrings> {
         // aa
         return Testsuite.<PalindromicSubstrings>builder()
                 .add(ExpectedTestcase.of(3, s -> s.countSubstrings("abc")))
+                .add(ExpectedTestcase.of(19, s -> s.countSubstrings("bcbcbbbcb")))
+                .add(ExpectedTestcase.of(9, s -> s.countSubstrings("bcbcb")))
+                .add(ExpectedTestcase.of(15, s -> s.countSubstrings("aaaaa")))
                 .add(ExpectedTestcase.of(7, s -> s.countSubstrings("abcba")))
                 .add(ExpectedTestcase.of(6, s -> s.countSubstrings("aaa")))
                 .add(ExpectedTestcase.of(6, s -> s.countSubstrings("fdsklf")))
                 .add(ExpectedTestcase.of(13, s -> s.countSubstrings("zabcccba")))
                 .add(ExpectedTestcase.of(17, s -> s.countSubstrings("zabccccba")))
                 .add(ExpectedTestcase.of(13, s -> s.countSubstrings("abcdddd")))
+                .add(ExpectedTestcase.of(64, s -> s.countSubstrings("bbccaacacdbdbcbcbbbcbadcbdddbabaddbcadb")))
                 .build();
     }
 
