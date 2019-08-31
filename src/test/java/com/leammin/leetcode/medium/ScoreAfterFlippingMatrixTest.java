fc5e19252df53fd1475b19d5420f9799df3ea8c9
Leammin <leammin@foxmail.com>
2019-08-31 17:18:47 +0800

    翻转矩阵后的得分

---
+++ src/test/java/com/leammin/leetcode/medium/ScoreAfterFlippingMatrixTest.java
@@ -0,0 +1,23 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.undone.medium.ScoreAfterFlippingMatrix;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2019-08-30
+ */
+class ScoreAfterFlippingMatrixTest extends AbstractTest<ScoreAfterFlippingMatrix> {
+    @Override
+    protected Testsuite<ScoreAfterFlippingMatrix> testsuite() {
+        return Testsuite.<ScoreAfterFlippingMatrix>builder()
+                .add(ExpectedTestcase.of(39, t -> t.matrixScore(new int[][]{
+                        new int[]{0, 0, 1, 1},
+                        new int[]{1, 0, 1, 0},
+                        new int[]{1, 1, 0, 0},
+                })))
+                .build();
+    }
+}
