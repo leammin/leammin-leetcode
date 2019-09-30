74d78081809cc88a04e50ca3917f46ef25458328
Leammin <leammin@foxmail.com>
2019-09-30 22:27:36 +0800

    唯一摩尔斯密码词

---
+++ src/main/java/com/leammin/leetcode/easy/SameTree.java
@@ -0,0 +1,64 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.struct.TreeNode;
+
+/**
+ * 100. 相同的树
+ * 
+ * <p>给定两个二叉树，编写一个函数来检验它们是否相同。</p>
+ * 
+ * <p>如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。</p>
+ * 
+ * <p><strong>示例&nbsp;1:</strong></p>
+ * 
+ * <pre><strong>输入: </strong>      1         1
+ *           / \       / \
+ *          2   3     2   3
+ * 
+ *         [1,2,3],   [1,2,3]
+ * 
+ * <strong>输出:</strong> true</pre>
+ * 
+ * <p><strong>示例 2:</strong></p>
+ * 
+ * <pre><strong>输入:  </strong>    1          1
+ *           /           \
+ *          2             2
+ * 
+ *         [1,2],     [1,null,2]
+ * 
+ * <strong>输出:</strong> false
+ * </pre>
+ * 
+ * <p><strong>示例&nbsp;3:</strong></p>
+ * 
+ * <pre><strong>输入:</strong>       1         1
+ *           / \       / \
+ *          2   1     1   2
+ * 
+ *         [1,2,1],   [1,1,2]
+ * 
+ * <strong>输出:</strong> false
+ * </pre>
+ * 
+ * 
+ * @author Leammin
+ * @date 2019-09-22
+ */
+public interface SameTree {
+    boolean isSameTree(TreeNode p, TreeNode q);
+
+    class Solution implements SameTree {
+
+        @Override
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == q) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
