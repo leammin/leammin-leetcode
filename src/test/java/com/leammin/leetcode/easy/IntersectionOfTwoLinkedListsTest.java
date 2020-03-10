64f0b0b4389a3808212589368771bf3d9e8e66ff
Leammin <leammin@foxmail.com>
2020-03-10 23:38:11 +0800

    相交链表

---
+++ src/test/java/com/leammin/leetcode/easy/IntersectionOfTwoLinkedListsTest.java
@@ -0,0 +1,34 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.struct.ListNode;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.ListNodeUtils;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-03-10
+ */
+class IntersectionOfTwoLinkedListsTest extends AbstractTest<IntersectionOfTwoLinkedLists> {
+    @Override
+    protected Testsuite<IntersectionOfTwoLinkedLists> testsuite() {
+        ListNode case1 = ListNode.of(8, 4, 5);
+        ListNode case1A = ListNode.of(4, 1);
+        ListNodeUtils.tail(case1A).next = case1;
+        ListNode case1B = ListNode.of(5, 0, 1);
+        ListNodeUtils.tail(case1B).next = case1;
+
+        ListNode case2 = ListNode.of(2, 4);
+        ListNode case2A = ListNode.of(0, 9, 1);
+        ListNodeUtils.tail(case2A).next = case2;
+        ListNode case2B = ListNode.of(3);
+        ListNodeUtils.tail(case2B).next = case2;
+
+        return Testsuite.<IntersectionOfTwoLinkedLists>builder()
+                .add(ExpectedTestcase.of(case1, t -> t.getIntersectionNode(case1A, case1B)))
+                .add(ExpectedTestcase.of(case2, t -> t.getIntersectionNode(case2A, case2B)))
+                .add(ExpectedTestcase.of(null, t -> t.getIntersectionNode(ListNode.of(2, 6, 4), ListNode.of(1, 5))))
+                .build();
+    }
+}
