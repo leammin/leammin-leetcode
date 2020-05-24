3259d636a7f7a224327c57987bd3da6f7af2783f
Leammin <leammin@foxmail.com>
2020-05-24 21:37:40 +0800

    课程表 II

---
+++ src/test/java/com/leammin/leetcode/hard/MedianOfTwoSortedArraysTest.java
@@ -0,0 +1,17 @@
+package com.leammin.leetcode.hard;
+
+import com.leammin.leetcode.undone.hard.MedianOfTwoSortedArrays;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-05-24
+ */
+class MedianOfTwoSortedArraysTest extends AbstractTest<MedianOfTwoSortedArrays> {
+    @Override
+    protected Testsuite<MedianOfTwoSortedArrays> testsuite() {
+        return Testsuite.<MedianOfTwoSortedArrays>builder()
+                .build();
+    }
+}
--- src/test/java/com/leammin/leetcode/medium/CourseScheduleIiTest.java
+++ src/test/java/com/leammin/leetcode/medium/CourseScheduleIiTest.java
@@ -1,7 +1,7 @@
 package com.leammin.leetcode.medium;
 
-import com.leammin.leetcode.undone.medium.CourseScheduleIi;
 import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
 import com.leammin.leetcode.util.Testsuite;
 
 /**
@@ -12,6 +12,9 @@ class CourseScheduleIiTest extends AbstractTest<CourseScheduleIi> {
     @Override
     protected Testsuite<CourseScheduleIi> testsuite() {
         return Testsuite.<CourseScheduleIi>builder()
+                .add(ExpectedTestcase.of(t -> t.findOrder(2, new int[][]{{1, 0}}), new int[]{0, 1}))
+                .add(ExpectedTestcase.of(t -> t.findOrder(4, new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}}), new int[]{0, 1, 2, 3}))
+                .add(ExpectedTestcase.of(t -> t.findOrder(2, new int[][]{{1, 0}, {0, 1}}), new int[0]))
                 .build();
     }
 }
