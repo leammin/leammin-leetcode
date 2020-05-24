f5a10df3d50d2121ef512a441606a0d3d909e8db
Leammin <leammin@foxmail.com>
2020-05-24 21:38:38 +0800

    最小覆盖子串 undone

---
+++ src/test/java/com/leammin/leetcode/hard/MinimumWindowSubstringTest.java
@@ -0,0 +1,17 @@
+package com.leammin.leetcode.hard;
+
+import com.leammin.leetcode.undone.hard.MinimumWindowSubstring;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-05-24
+ */
+class MinimumWindowSubstringTest extends AbstractTest<MinimumWindowSubstring> {
+    @Override
+    protected Testsuite<MinimumWindowSubstring> testsuite() {
+        return Testsuite.<MinimumWindowSubstring>builder()
+                .build();
+    }
+}
