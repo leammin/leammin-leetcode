bc0bb053d6a239df1d4d5e228baef854717e9d2f
Leammin <leammin@foxmail.com>
2020-05-29 00:47:31 +0800

    打家劫舍

---
+++ src/test/java/com/leammin/leetcode/easy/HouseRobberTest.java
@@ -0,0 +1,19 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-05-29
+ */
+class HouseRobberTest extends AbstractTest<HouseRobber> {
+    @Override
+    protected Testsuite<HouseRobber> testsuite() {
+        return Testsuite.<HouseRobber>builder()
+                .add(ExpectedTestcase.of(t -> t.rob(new int[]{1, 2, 3, 1}), 4))
+                .add(ExpectedTestcase.of(t -> t.rob(new int[]{2, 7, 9, 3, 1}), 12))
+                .build();
+    }
+}
