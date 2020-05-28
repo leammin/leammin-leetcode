ed060a32906110a27b062196eaf1ca19f46a306e
Leammin <leammin@foxmail.com>
2020-05-28 23:42:42 +0800

    数组中的逆序对 undone

---
+++ src/test/java/com/leammin/leetcode/hard/ShuZuZhongDeNiXuDuiLcofTest.java
@@ -0,0 +1,17 @@
+package com.leammin.leetcode.hard;
+
+import com.leammin.leetcode.undone.hard.ShuZuZhongDeNiXuDuiLcof;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-05-28
+ */
+class ShuZuZhongDeNiXuDuiLcofTest extends AbstractTest<ShuZuZhongDeNiXuDuiLcof> {
+    @Override
+    protected Testsuite<ShuZuZhongDeNiXuDuiLcof> testsuite() {
+        return Testsuite.<ShuZuZhongDeNiXuDuiLcof>builder()
+                .build();
+    }
+}
