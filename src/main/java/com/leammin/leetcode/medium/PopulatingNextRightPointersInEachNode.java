9e4288eaa8c22ada1cb6514dedb13fcf282838c8
Leammin <leammin@foxmail.com>
2020-06-03 23:23:09 +0800

    Node moved

--- src/main/java/com/leammin/leetcode/medium/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/com/leammin/leetcode/medium/PopulatingNextRightPointersInEachNode.java
@@ -1,6 +1,6 @@
 package com.leammin.leetcode.medium;
 
-import com.leammin.leetcode.struct.Node;
+import java.util.Objects;
 
 /**
  * 116. 填充每个节点的下一个右侧节点指针
@@ -105,6 +105,42 @@ public interface PopulatingNextRightPointersInEachNode {
                 }
             }
             return root;
+        }
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node left, Node right, Node next) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return val == node.val &&
+                    Objects.equals(left, node.left) &&
+                    Objects.equals(right, node.right) &&
+                    Objects.equals(next, node.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, left, right, next);
         }
     }
 }
--- src/test/java/com/leammin/leetcode/medium/PopulatingNextRightPointersInEachNodeTest.java
+++ src/test/java/com/leammin/leetcode/medium/PopulatingNextRightPointersInEachNodeTest.java
@@ -1,6 +1,5 @@
 package com.leammin.leetcode.medium;
 
-import com.leammin.leetcode.struct.Node;
 import com.leammin.leetcode.util.AbstractTest;
 import com.leammin.leetcode.util.ExpectedTestcase;
 import com.leammin.leetcode.util.Testsuite;
@@ -14,19 +13,19 @@ class PopulatingNextRightPointersInEachNodeTest extends AbstractTest<PopulatingN
     protected Testsuite<PopulatingNextRightPointersInEachNode> testsuite() {
         return Testsuite.<PopulatingNextRightPointersInEachNode>builder()
                 .add(ExpectedTestcase.ofSupplier(
-                        t -> t.connect(new Node(
+                        t -> t.connect(new PopulatingNextRightPointersInEachNode.Node(
                                 1,
-                                new Node(2, new Node(4), new Node(5), null),
-                                new Node(3, new Node(6), new Node(7), null),
+                                new PopulatingNextRightPointersInEachNode.Node(2, new PopulatingNextRightPointersInEachNode.Node(4), new PopulatingNextRightPointersInEachNode.Node(5), null),
+                                new PopulatingNextRightPointersInEachNode.Node(3, new PopulatingNextRightPointersInEachNode.Node(6), new PopulatingNextRightPointersInEachNode.Node(7), null),
                                 null
                         )), () -> {
-                            Node node7 = new Node(7);
-                            Node node6 = new Node(6, null, null, node7);
-                            Node node5 = new Node(5, null, null, node6);
-                            Node node4 = new Node(4, null, null, node5);
-                            Node node3 = new Node(3, node6, node7, null);
-                            Node node2 = new Node(2, node4, node5, node3);
-                            return new Node(1, node2, node3, null);
+                            PopulatingNextRightPointersInEachNode.Node node7 = new PopulatingNextRightPointersInEachNode.Node(7);
+                            PopulatingNextRightPointersInEachNode.Node node6 = new PopulatingNextRightPointersInEachNode.Node(6, null, null, node7);
+                            PopulatingNextRightPointersInEachNode.Node node5 = new PopulatingNextRightPointersInEachNode.Node(5, null, null, node6);
+                            PopulatingNextRightPointersInEachNode.Node node4 = new PopulatingNextRightPointersInEachNode.Node(4, null, null, node5);
+                            PopulatingNextRightPointersInEachNode.Node node3 = new PopulatingNextRightPointersInEachNode.Node(3, node6, node7, null);
+                            PopulatingNextRightPointersInEachNode.Node node2 = new PopulatingNextRightPointersInEachNode.Node(2, node4, node5, node3);
+                            return new PopulatingNextRightPointersInEachNode.Node(1, node2, node3, null);
                         }
                 ))
                 .build();
