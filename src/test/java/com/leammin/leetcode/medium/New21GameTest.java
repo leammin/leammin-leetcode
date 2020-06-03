dfc2fb6b26f65df5e0eae6273820c9c1ca6a2e7f
Leammin <836842647@qq.com>
2020-06-03 21:50:40 +0800

    新21点 undone

---
+++ src/test/java/com/leammin/leetcode/medium/New21GameTest.java
@@ -0,0 +1,21 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.undone.medium.New21Game;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-06-03
+ */
+class New21GameTest extends AbstractTest<New21Game> {
+    @Override
+    protected Testsuite<New21Game> testsuite() {
+        return Testsuite.<New21Game>builder()
+                .add(ExpectedTestcase.of(t -> t.new21Game(10, 1, 10), 1.0))
+                .add(ExpectedTestcase.of(t -> t.new21Game(6, 1, 10), 0.6))
+                .add(ExpectedTestcase.of(t -> t.new21Game(21, 17, 10), 0.73278))
+                .build(-1);
+    }
+}
