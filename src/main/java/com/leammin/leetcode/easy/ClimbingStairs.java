6de2c2a574d532cde603e300cf0f481bd816abb0
Leammin <836842647@qq.com>
2020-06-13 15:32:35 +0800

    爬楼梯

--- src/main/java/com/leammin/leetcode/easy/ClimbingStairs.java
+++ src/main/java/com/leammin/leetcode/easy/ClimbingStairs.java
@@ -61,5 +61,18 @@ public interface ClimbingStairs {
         }
     }
 
+    class Solution2 implements ClimbingStairs {
 
+        @Override
+        public int climbStairs(int n) {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++) {
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+            return b;
+        }
+    }
 }
