f289aa59f43be7779cc26897563a676d7f19506e
Leammin <836842647@qq.com>
2020-06-14 15:48:36 +0800

    最大连续1的个数

---
+++ src/test/java/com/leammin/leetcode/easy/MaxConsecutiveOnesTest.java
@@ -0,0 +1,18 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-06-13
+ */
+class MaxConsecutiveOnesTest extends AbstractTest<MaxConsecutiveOnes> {
+    @Override
+    protected Testsuite<MaxConsecutiveOnes> testsuite() {
+        return Testsuite.<MaxConsecutiveOnes>builder()
+                .add(ExpectedTestcase.of(t -> t.findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1}), 3))
+                .build();
+    }
+}
