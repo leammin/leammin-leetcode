5906f5517ba9bff0ef6ba06aa21ef8d24e40e562
Leammin <leammin@foxmail.com>
2020-06-14 21:53:20 +0800

    转变数组后最接近目标值的数组和

---
+++ src/test/java/com/leammin/leetcode/medium/SumOfMutatedArrayClosestToTargetTest.java
@@ -0,0 +1,20 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-06-14
+ */
+class SumOfMutatedArrayClosestToTargetTest extends AbstractTest<SumOfMutatedArrayClosestToTarget> {
+    @Override
+    protected Testsuite<SumOfMutatedArrayClosestToTarget> testsuite() {
+        return Testsuite.<SumOfMutatedArrayClosestToTarget>builder()
+                .add(ExpectedTestcase.of(t -> t.findBestValue(new int[]{4, 9, 3}, 10), 3))
+                .add(ExpectedTestcase.of(t -> t.findBestValue(new int[]{2, 3, 5}, 10), 5))
+                .add(ExpectedTestcase.of(t -> t.findBestValue(new int[]{60864, 25176, 27249, 21296, 20204}, 56803), 11361))
+                .build();
+    }
+}
