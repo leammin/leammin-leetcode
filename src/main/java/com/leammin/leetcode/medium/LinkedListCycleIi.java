1518f115765d8a884ca78295ae8afd3e0264c3c2
Leammin <836842647@qq.com>
2020-06-16 18:59:13 +0800

    环形链表 II

---
+++ src/main/java/com/leammin/leetcode/medium/LinkedListCycleIi.java
@@ -0,0 +1,82 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.struct.ListNode;
+
+/**
+ * 142. 环形链表 II
+ * 
+ * <p>给定一个链表，返回链表开始入环的第一个节点。&nbsp;如果链表无环，则返回&nbsp;<code>null</code>。</p>
+ * 
+ * <p>为了表示给定链表中的环，我们使用整数 <code>pos</code> 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 <code>pos</code> 是 <code>-1</code>，则在该链表中没有环。</p>
+ * 
+ * <p><strong>说明：</strong>不允许修改给定的链表。</p>
+ * 
+ * <p>&nbsp;</p>
+ * 
+ * <p><strong>示例 1：</strong></p>
+ * 
+ * <pre><strong>输入：</strong>head = [3,2,0,-4], pos = 1
+ * <strong>输出：</strong>tail connects to node index 1
+ * <strong>解释：</strong>链表中有一个环，其尾部连接到第二个节点。
+ * </pre>
+ * 
+ * <p><img alt="" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/07/circularlinkedlist.png" style="height: 97px; width: 300px;"></p>
+ * 
+ * <p><strong>示例&nbsp;2：</strong></p>
+ * 
+ * <pre><strong>输入：</strong>head = [1,2], pos = 0
+ * <strong>输出：</strong>tail connects to node index 0
+ * <strong>解释：</strong>链表中有一个环，其尾部连接到第一个节点。
+ * </pre>
+ * 
+ * <p><img alt="" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/07/circularlinkedlist_test2.png" style="height: 74px; width: 141px;"></p>
+ * 
+ * <p><strong>示例 3：</strong></p>
+ * 
+ * <pre><strong>输入：</strong>head = [1], pos = -1
+ * <strong>输出：</strong>no cycle
+ * <strong>解释：</strong>链表中没有环。
+ * </pre>
+ * 
+ * <p><img alt="" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/07/circularlinkedlist_test3.png" style="height: 45px; width: 45px;"></p>
+ * 
+ * <p>&nbsp;</p>
+ * 
+ * <p><strong>进阶：</strong><br>
+ * 你是否可以不用额外空间解决此题？</p>
+ * 
+ * 
+ * @author Leammin
+ * @date 2020-06-16
+ */
+public interface LinkedListCycleIi {
+    ListNode detectCycle(ListNode head);
+
+    class Solution implements LinkedListCycleIi {
+
+        @Override
+        public ListNode detectCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return null;
+            }
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    break;
+                }
+            }
+            if (fast != slow) {
+                return null;
+            }
+            slow = head;
+            while (fast != slow) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+}
---
+++ src/test/java/com/leammin/leetcode/medium/LinkedListCycleIiTest.java
@@ -0,0 +1,26 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.struct.ListNode;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.ListNodeUtils;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-06-16
+ */
+class LinkedListCycleIiTest extends AbstractTest<LinkedListCycleIi> {
+    @Override
+    protected Testsuite<LinkedListCycleIi> testsuite() {
+        return Testsuite.<LinkedListCycleIi>builder()
+                .add(ExpectedTestcase.of(t -> {
+                    ListNode list = ListNode.of(2, 0, -4);
+                    ListNodeUtils.tail(list).next = list;
+                    ListNode head = ListNode.of(3);
+                    head.next = list;
+                    return t.detectCycle(head).val;
+                }, 2))
+                .build();
+    }
+}
