0a0415c050cd93cb1ea1a65797efd897c38b3ef4
Leammin <leammin@foxmail.com>
2020-06-26 21:43:45 +0800

    单词拆分

---
+++ src/test/java/com/leammin/leetcode/medium/WordBreakTest.java
@@ -0,0 +1,23 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+import java.util.List;
+
+/**
+ * @author Leammin
+ * @date 2020-06-25
+ */
+class WordBreakTest extends AbstractTest<WordBreak> {
+    @Override
+    protected Testsuite<WordBreak> testsuite() {
+        return Testsuite.<WordBreak>builder()
+                .add(ExpectedTestcase.of(t -> t.wordBreak("leetcode", List.of("leet", "code")), true))
+                .add(ExpectedTestcase.of(t -> t.wordBreak("applepenapple", List.of("apple", "pen")), true))
+                .add(ExpectedTestcase.of(t -> t.wordBreak("catsandog", List.of("cats", "dog", "sand", "and", "cat")), false))
+                .add(ExpectedTestcase.of(t -> t.wordBreak("bb", List.of("a","b","bbb","bbbb")), true))
+                .build();
+    }
+}
