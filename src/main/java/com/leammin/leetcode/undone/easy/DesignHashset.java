8abe5b05b33d60f2787ad6228665172e25e9d035
Leammin <leammin@foxmail.com>
2020-06-27 22:21:53 +0800

    设计哈希 undone

---
+++ src/main/java/com/leammin/leetcode/undone/easy/DesignHashset.java
@@ -0,0 +1,68 @@
+package com.leammin.leetcode.undone.easy;
+
+/**
+ * 705. 设计哈希集合
+ * 
+ * <p>不使用任何内建的哈希表库设计一个哈希集合</p>
+ * 
+ * <p>具体地说，你的设计应该包含以下的功能</p>
+ * 
+ * <ul>
+ * 	<li><code>add(value)</code>：向哈希集合中插入一个值。</li>
+ * 	<li><code>contains(value)</code> ：返回哈希集合中是否存在这个值。</li>
+ * 	<li><code>remove(value)</code>：将给定值从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。</li>
+ * </ul>
+ * 
+ * <p><br>
+ * <strong>示例:</strong></p>
+ * 
+ * <pre>MyHashSet hashSet = new MyHashSet();
+ * hashSet.add(1); &nbsp; &nbsp; &nbsp; &nbsp; 
+ * hashSet.add(2); &nbsp; &nbsp; &nbsp; &nbsp; 
+ * hashSet.contains(1); &nbsp;&nbsp;&nbsp;// 返回 true
+ * hashSet.contains(3); &nbsp;&nbsp;&nbsp;// 返回 false (未找到)
+ * hashSet.add(2); &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+ * hashSet.contains(2); &nbsp;&nbsp;&nbsp;// 返回 true
+ * hashSet.remove(2); &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+ * hashSet.contains(2); &nbsp;&nbsp;&nbsp;// 返回  false (已经被删除)
+ * </pre>
+ * 
+ * <p><br>
+ * <strong>注意：</strong></p>
+ * 
+ * <ul>
+ * 	<li>所有的值都在&nbsp;<code>[0, 1000000]</code>的范围内。</li>
+ * 	<li>操作的总数目在<code>[1, 10000]</code>范围内。</li>
+ * 	<li>不要使用内建的哈希集合库。</li>
+ * </ul>
+ * 
+ * 
+ * @author Leammin
+ * @date 2020-06-27
+ */
+public interface DesignHashset {
+    void add(int key);
+
+    void remove(int key);
+
+    boolean contains(int key);
+
+    class Solution implements DesignHashset {
+        boolean[] hash = new boolean[1000001];
+
+        @Override
+        public void add(int key) {
+            hash[key] = true;
+        }
+
+        @Override
+        public void remove(int key) {
+            hash[key] = false;
+        }
+
+        @Override
+        public boolean contains(int key) {
+            return hash[key];
+        }
+    }
+}
---
+++ src/test/java/com/leammin/leetcode/easy/DesignHashmapTest.java
@@ -0,0 +1,36 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.undone.easy.DesignHashmap;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testcase;
+import com.leammin.leetcode.util.Testsuite;
+import org.assertj.core.api.Assertions;
+
+/**
+ * @author Leammin
+ * @date 2020-06-27
+ */
+class DesignHashmapTest extends AbstractTest<DesignHashmap> {
+    @Override
+    protected Testsuite<DesignHashmap> testsuite() {
+        return Testsuite.<DesignHashmap>builder()
+                .add(Testcase.of(t -> {
+                    long b = System.nanoTime();
+                    t.put(1, 1);
+                    t.put(2, 2);
+                    int r1 = t.get(1);// 返回 1
+                    int r2 = t.get(3);            // 返回 -1 (未找到)
+                    t.put(2, 1);         // 更新已有的值
+                    int r3 = t.get(2);            // 返回 1
+                    t.remove(2);         // 删除键为2的数据
+                    int r4 = t.get(2);            // 返回 -1 (未找到)
+                    long a = System.nanoTime();
+                    Assertions.assertThat(r1).isEqualTo(1);
+                    Assertions.assertThat(r2).isEqualTo(-1);
+                    Assertions.assertThat(r3).isEqualTo(1);
+                    Assertions.assertThat(r4).isEqualTo(-1);
+                    return a - b;
+                }))
+                .build();
+    }
+}
