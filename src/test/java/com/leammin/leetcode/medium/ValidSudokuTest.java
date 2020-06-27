7ef635f3c0087cc5ce8fbe092c9749b66910d8a3
Leammin <leammin@foxmail.com>
2020-06-28 01:07:30 +0800

    ValidSudokuTest

---
+++ src/test/java/com/leammin/leetcode/medium/ValidSudokuTest.java
@@ -0,0 +1,39 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-06-28
+ */
+class ValidSudokuTest extends AbstractTest<ValidSudoku> {
+    @Override
+    protected Testsuite<ValidSudoku> testsuite() {
+        return Testsuite.<ValidSudoku>builder()
+                .add(ExpectedTestcase.of(t -> t.isValidSudoku(new char[][]{
+                        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+                }), true))
+                .add(ExpectedTestcase.of(t -> t.isValidSudoku(new char[][]{
+                        {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+                }), false))
+                .build();
+    }
+}
