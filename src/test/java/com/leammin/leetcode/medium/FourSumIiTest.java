84360d11cfc230e85b85ef1ac6ae2438068468b4
Leammin <leammin@foxmail.com>
2020-06-28 22:14:10 +0800

    四数相加 II

---
+++ src/test/java/com/leammin/leetcode/medium/FourSumIiTest.java
@@ -0,0 +1,18 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-06-28
+ */
+class FourSumIiTest extends AbstractTest<FourSumIi> {
+    @Override
+    protected Testsuite<FourSumIi> testsuite() {
+        return Testsuite.<FourSumIi>builder()
+                .add(ExpectedTestcase.of(t -> t.fourSumCount(new int[]{1, 2}, new int[]{-2, -1}, new int[]{-1, 2}, new int[]{0, 2}), 2))
+                .build();
+    }
+}
