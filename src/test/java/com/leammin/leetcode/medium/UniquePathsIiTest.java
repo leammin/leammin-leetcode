8aef447e0508b95cc986a4215d40b3bfaf345b66
Leammin <836842647@qq.com>
2020-07-06 22:36:19 +0800

    不同路径 II undone

---
+++ src/test/java/com/leammin/leetcode/medium/UniquePathsIiTest.java
@@ -0,0 +1,32 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.undone.medium.UniquePathsIi;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2020-07-06
+ */
+class UniquePathsIiTest extends AbstractTest<UniquePathsIi> {
+    @Override
+    protected Testsuite<UniquePathsIi> testsuite() {
+        return Testsuite.<UniquePathsIi>builder()
+                .add(ExpectedTestcase.of(t -> t.uniquePathsWithObstacles(new int[][]{
+                        {0, 0, 0},
+                        {0, 1, 0},
+                        {0, 0, 0}
+                }), 2))
+                .add(ExpectedTestcase.of(t -> t.uniquePathsWithObstacles(new int[][]{
+                        {0},
+                }), 1))
+                .add(ExpectedTestcase.of(t -> t.uniquePathsWithObstacles(new int[][]{
+                        {1},
+                }), 0))
+                .add(ExpectedTestcase.of(t -> t.uniquePathsWithObstacles(new int[][]{
+                        {0,1},
+                }), 0))
+                .build();
+    }
+}
