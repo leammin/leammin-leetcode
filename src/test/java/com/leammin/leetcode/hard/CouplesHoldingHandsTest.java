c775ab711ef6ccb7f986d47322109890e7adae71
Leammin <leammin@foxmail.com>
2021-02-14 22:26:34 +0800

    情侣牵手

---
+++ src/test/java/com/leammin/leetcode/hard/CouplesHoldingHandsTest.java
@@ -0,0 +1,20 @@
+package com.leammin.leetcode.hard;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2021-02-14
+ */
+class CouplesHoldingHandsTest extends AbstractTest<CouplesHoldingHands> {
+    @Override
+    protected Testsuite<CouplesHoldingHands> testsuite() {
+        return Testsuite.<CouplesHoldingHands>builder()
+                .add(ExpectedTestcase.of(t -> t.minSwapsCouples(new int[]{0, 2, 1, 3}), 1))
+                .add(ExpectedTestcase.of(t -> t.minSwapsCouples(new int[]{3, 2, 0, 1}), 0))
+                .build();
+    }
+
+}
