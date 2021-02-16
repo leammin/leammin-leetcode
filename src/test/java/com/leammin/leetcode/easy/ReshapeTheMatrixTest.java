24501366942e6b9b79ef53e987902162ea6b8952
Leammin <leammin@foxmail.com>
2021-02-17 03:13:51 +0800

    重塑矩阵

---
+++ src/test/java/com/leammin/leetcode/easy/ReshapeTheMatrixTest.java
@@ -0,0 +1,20 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.undone.easy.ReshapeTheMatrix;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.ExpectedTestcase;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2021-02-17
+ */
+class ReshapeTheMatrixTest extends AbstractTest<ReshapeTheMatrix> {
+    @Override
+    protected Testsuite<ReshapeTheMatrix> testsuite() {
+        return Testsuite.<ReshapeTheMatrix>builder()
+                .add(ExpectedTestcase.of(t -> t.matrixReshape(new int[][]{{1, 2}, {3, 4}}, 1, 4), new int[][]{{1, 2, 3, 4}}))
+                .add(ExpectedTestcase.of(t -> t.matrixReshape(new int[][]{{1, 2}, {3, 4}}, 2, 4), new int[][]{{1, 2}, {3, 4}}))
+                .build();
+    }
+}
