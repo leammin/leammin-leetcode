84c49d5b11c19c8d891bbede22903d24acca42d8
Leammin <leammin@foxmail.com>
2021-04-22 20:34:59 +0800

    binarySearch done

--- src/main/java/com/leammin/leetcode/util/Codes.java
+++ src/main/java/com/leammin/leetcode/util/Codes.java
@@ -1,6 +1,6 @@
 package com.leammin.leetcode.util;
 
-import java.util.Arrays;
+import com.google.common.base.Preconditions;
 
 /**
  * @author xiebao.llm
@@ -14,42 +14,79 @@ public interface Codes {
          * 参考源码 {@link java.util.Arrays#binarySearch(int[], int, int, int)}
          */
         public int binarySearch(int[] nums, int from, int to, int key) {
-            int low = from;
-            int high = to - 1;
-
-            while (low <= high) {
-                int mid = (low + high) >>> 1;
-                int midVal = nums[mid];
-
-                if (midVal < key) {
-                    low = mid + 1;
-                } else if (midVal > key) {
-                    high = mid - 1;
+            int l = from;
+            int r = to - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] < key) {
+                    l = mid + 1;
+                } else if (nums[mid] > key) {
+                    r = mid - 1;
                 } else {
-                    return mid; // key found
+                    return mid;
                 }
             }
-            return -(low + 1);  // key not found.
+            return -(l + 1);
         }
 
         /**
          * 二分查找，当有多个key时，返回左边界，否则返回插入点。
          */
         public int binarySearchLeft(int[] nums, int from, int to, int key) {
-            int low = from;
-            int high = to - 1;
-
-            while (low <= high) {
-                int mid = (low + high) >>> 1;
-                int midVal = nums[mid];
-
-                if (midVal < key) {
-                    low = mid + 1;
-                } else if (midVal > key) {
-                    high = mid - 1;
+            int l = from;
+            int r = to - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] >= key) {  //这里需要注意
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
                 }
             }
-            return -(low + 1);
+            return l < to && nums[l] == key ? l : (-l - 1);
+        }
+
+        /**
+         * 二分查找，当有多个key时，返回右边界，否则返回插入点。
+         */
+        public int binarySearchRight(int[] nums, int from, int to, int key) {
+            int l = from;
+            int r = to - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] > key) {  //这里需要注意
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            return r >= from && nums[r] == key ? r : (-r - 2);
+        }
+
+        public static void main(String[] args) {
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 0, 5, 3) == 1);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 0, 5, 5) == 4);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 0, 5, 1) == 0);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 0, 5, 2) == -2);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 0, 5, 4) == -5);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 0, 5, 0) == -1);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 0, 5, 6) == -6);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 1, 5, 1) == -2);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 1, 4, 3) == 1);
+            Preconditions.checkArgument(new BinarySearch().binarySearchLeft(new int[] {1, 3, 3, 3, 5}, 1, 4, 5) == -5);
+
+
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 0, 5, 3) == 3);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 0, 5, 1) == 0);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 0, 5, 5) == 4);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 0, 5, 2) == -2);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 0, 5, 4) == -5);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 0, 5, 0) == -1);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 0, 5, 6) == -6);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 1, 5, 1) == -2);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 1, 4, 5) == -5);
+            Preconditions.checkArgument(new BinarySearch().binarySearchRight(new int[] {1, 3, 3, 3, 5}, 1, 4, 3) == 3);
+
         }
     }
 }
