847f8a15e631bc51de99f48aa825ee491cf5d0fc
leammin <leammin@foxmail.com>
2021-08-10 09:04:02 +0800

    最后一个单词的长度 删除排序链表中的重复元素

---
+++ src/test/java/com/leammin/leetcode/easy/LengthOfLastWordTest.java
@@ -0,0 +1,20 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2021-08-10
+ */
+class LengthOfLastWordTest extends AbstractTest<LengthOfLastWord> {
+    @Override
+    protected Testsuite<LengthOfLastWord> testsuite() {
+        return Testsuite.<LengthOfLastWord>builder()
+                .addExpected(t -> t.lengthOfLastWord("Hello World"), 5)
+                .addExpected(t -> t.lengthOfLastWord("HelloWorld"), 10)
+                .addExpected(t -> t.lengthOfLastWord("HelloWorld "), 10)
+                .addExpected(t -> t.lengthOfLastWord(" HelloWorld"), 10)
+                .build();
+    }
+}
