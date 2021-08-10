0f24783b95279f0419e74d987f327f2a5f9090a1
leammin <leammin@foxmail.com>
2021-08-10 08:43:58 +0800

    方阵中战斗力最弱的 K 行

---
+++ src/test/java/com/leammin/leetcode/easy/TheKWeakestRowsInAMatrixTest.java
@@ -0,0 +1,33 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.undone.easy.TheKWeakestRowsInAMatrix;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2021-08-01
+ */
+class TheKWeakestRowsInAMatrixTest extends AbstractTest<TheKWeakestRowsInAMatrix> {
+    @Override
+    protected Testsuite<TheKWeakestRowsInAMatrix> testsuite() {
+        return Testsuite.<TheKWeakestRowsInAMatrix>builder()
+                .addExpected(t -> t.kWeakestRows(
+                        new int[][]{
+                                {1, 1, 0, 0, 0},
+                                {1, 1, 1, 1, 0},
+                                {1, 0, 0, 0, 0},
+                                {1, 1, 0, 0, 0},
+                                {1, 1, 1, 1, 1}
+                        }, 3), new int[]{2, 0, 3})
+                .addExpected(t -> t.kWeakestRows(
+                        new int[][]{
+                                {1, 0, 0, 0},
+                                {1, 1, 1, 1},
+                                {1, 0, 0, 0},
+                                {1, 0, 0, 0}
+                        }, 2), new int[]{0,2})
+                .build();
+    }
+
+}
