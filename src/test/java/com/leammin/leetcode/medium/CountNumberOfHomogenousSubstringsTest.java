86b8262c3e379ebc70cf08de43bd89cf7b3efba1
liming.liu <liming.liu@shopee.com>
2023-01-18 16:03:40 +0800

    CountNumberOfHomogenousSubstrings

---
+++ src/test/java/com/leammin/leetcode/medium/CountNumberOfHomogenousSubstringsTest.java
@@ -0,0 +1,20 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.undone.medium.CountNumberOfHomogenousSubstrings;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2022-12-26
+ */
+class CountNumberOfHomogenousSubstringsTest extends AbstractTest<CountNumberOfHomogenousSubstrings> {
+    @Override
+    protected Testsuite<CountNumberOfHomogenousSubstrings> testsuite() {
+        return Testsuite.<CountNumberOfHomogenousSubstrings>builder()
+                .addExpected(t -> t.countHomogenous("abbcccaa"), 13)
+                .addExpected(t -> t.countHomogenous("xy"), 2)
+                .addExpected(t -> t.countHomogenous("zzzzz"), 15)
+                .build();
+    }
+}
