d905b2975f62e0bca56d50216302b78e3ee28084
liming.liu <liming.liu@shopee.com>
2023-01-19 15:55:58 +0800

    StrongPasswordCheckerIi

---
+++ src/test/java/com/leammin/leetcode/easy/StrongPasswordCheckerIiTest.java
@@ -0,0 +1,19 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2023-01-19
+ */
+class StrongPasswordCheckerIiTest extends AbstractTest<StrongPasswordCheckerIi> {
+    @Override
+    protected Testsuite<StrongPasswordCheckerIi> testsuite() {
+        return Testsuite.<StrongPasswordCheckerIi>builder()
+                .addExpected(t -> t.strongPasswordCheckerII("IloveLe3tcode!"), true)
+                .addExpected(t -> t.strongPasswordCheckerII("Me+You--IsMyDream"), false)
+                .addExpected(t -> t.strongPasswordCheckerII("1aB!"), false)
+                .build();
+    }
+}
