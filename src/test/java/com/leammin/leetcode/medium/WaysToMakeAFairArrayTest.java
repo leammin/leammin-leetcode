b1f46d66ff0f02cf9463479734cd1f31f2d524af
liming.liu <liming.liu@shopee.com>
2023-01-28 16:50:46 +0800

    WaysToMakeAFairArray

---
+++ src/test/java/com/leammin/leetcode/medium/WaysToMakeAFairArrayTest.java
@@ -0,0 +1,20 @@
+package com.leammin.leetcode.medium;
+
+import com.leammin.leetcode.undone.medium.WaysToMakeAFairArray;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2023-01-28
+ */
+class WaysToMakeAFairArrayTest extends AbstractTest<WaysToMakeAFairArray> {
+    @Override
+    protected Testsuite<WaysToMakeAFairArray> testsuite() {
+        return Testsuite.<WaysToMakeAFairArray>builder()
+                .addExpected(t -> t.waysToMakeFair(new int[]{2,1,6,4}), 1)
+                .addExpected(t -> t.waysToMakeFair(new int[]{1,1,1}), 3)
+                .addExpected(t -> t.waysToMakeFair(new int[]{1,2,3}), 0)
+                .build();
+    }
+}
