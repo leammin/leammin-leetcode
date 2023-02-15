06f4d017e51ccd7fdefb4e5e0828d5efb8b49bd7
leammin <leammin@foxmail.com>
2023-02-16 03:22:00 +0800

    leetcode

---
+++ src/test/java/com/leammin/leetcode/easy/MaximumNumberOfPairsInArrayTest.java
@@ -0,0 +1,19 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2023-02-16
+ */
+class MaximumNumberOfPairsInArrayTest extends AbstractTest<MaximumNumberOfPairsInArray> {
+    @Override
+    protected Testsuite<MaximumNumberOfPairsInArray> testsuite() {
+        return Testsuite.<MaximumNumberOfPairsInArray>builder()
+                .addExpected(t -> t.numberOfPairs(new int[]{1,3,2,1,3,2,2}), new int[]{3, 1})
+                .addExpected(t -> t.numberOfPairs(new int[]{1,1}), new int[]{1,0})
+                .addExpected(t -> t.numberOfPairs(new int[]{0}), new int[]{0,1})
+                .build();
+    }
+}
