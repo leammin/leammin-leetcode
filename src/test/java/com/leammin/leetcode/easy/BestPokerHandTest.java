3d7067099f1885deb274f5b4718f2303c886ab3b
leammin <leammin@foxmail.com>
2023-02-20 22:17:37 +0800

    leetcode

---
+++ src/test/java/com/leammin/leetcode/easy/BestPokerHandTest.java
@@ -0,0 +1,18 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.undone.easy.BestPokerHand;
+import com.leammin.leetcode.util.AbstractTest;
+import com.leammin.leetcode.util.Testsuite;
+
+/**
+ * @author Leammin
+ * @date 2023-02-20
+ */
+class BestPokerHandTest extends AbstractTest<BestPokerHand> {
+    @Override
+    protected Testsuite<BestPokerHand> testsuite() {
+        return Testsuite.<BestPokerHand>builder()
+                .addExpected(t -> t.bestHand(new int[]{13,2,3,1,9}, new char[]{'a', 'a', 'a', 'a', 'a'}), "Flush")
+                .build();
+    }
+}
