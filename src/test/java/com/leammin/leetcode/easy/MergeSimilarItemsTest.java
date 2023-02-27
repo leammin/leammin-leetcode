a704a213b8255accc96414523c90883b0cfc486a
leammin <leammin@foxmail.com>
2023-02-28 02:38:57 +0800

    MergeSimilarItems

---
+++ src/test/java/com/leammin/leetcode/easy/MergeSimilarItemsTest.java
@@ -0,0 +1,11 @@
+package com.leammin.leetcode.easy;
+
+import com.leammin.leetcode.undone.easy.MergeSimilarItems;
+import com.leammin.leetcode.util.AbstractTest;
+
+/**
+ * @author Leammin
+ * @date 2023-02-28
+ */
+class MergeSimilarItemsTest extends AbstractTest<MergeSimilarItems> {
+}
