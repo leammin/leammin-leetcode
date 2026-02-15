3c9724e53c4bc396852f1eb5cbbf2d02b3056aaf
liming.liu <liming.liu@shopee.com>
2026-02-15 22:06:31 +0800

    KThSmallestInLexicographicalOrder

---
+++ src/test/java/com/leammin/leetcode/hard/KThSmallestInLexicographicalOrderTest.java
@@ -0,0 +1,11 @@
+package com.leammin.leetcode.hard;
+
+import com.leammin.leetcode.undone.hard.KThSmallestInLexicographicalOrder;
+import com.leammin.leetcode.util.AbstractTest;
+
+/**
+ * @author Leammin
+ * @date 2025-06-09
+ */
+class KThSmallestInLexicographicalOrderTest extends AbstractTest<KThSmallestInLexicographicalOrder> {
+}
